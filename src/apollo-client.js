80d2e552c59573ec281d8d6034d933294b27f6b1
Thibault Guilhem <thibault.guilhem@gmail.com>
2024-08-28 11:23:01 +0200

    chore: Update package.json dependencies for dotenv to version 16.4.5

--- src/apollo-client.js
+++ src/apollo-client.js
@@ -1,9 +1,11 @@
 import { ApolloClient, InMemoryCache, HttpLink } from "@apollo/client/core";
 
+const KEYSTONE_API_URL = import.meta.env.VITE_KEYSTONE_API_URL;
+
 // Remplace l'URL par celle de ton serveur Keystone
 const httpLink = new HttpLink({
   uri:
-    process.env.VITE_KEYSTONE_API_URL ||
+    KEYSTONE_API_URL ||
     "https://keystone-back.admin.serv.thiserv.com/api/graphql",
   credentials: "include",
 });
