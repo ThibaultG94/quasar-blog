3878eb945acd5d3c973093de561a3896a3e16d36
Thibault Guilhem <thibault.guilhem@gmail.com>
2024-08-28 10:41:53 +0200

    chore: Update Apollo Client URL to use production endpoint

--- src/apollo-client.js
+++ src/apollo-client.js
@@ -2,7 +2,9 @@ import { ApolloClient, InMemoryCache, HttpLink } from "@apollo/client/core";
 
 // Remplace l'URL par celle de ton serveur Keystone
 const httpLink = new HttpLink({
-  uri: process.env.VITE_KEYSTONE_API_URL || "http://localhost:3000/api/graphql",
+  uri:
+    process.env.VITE_KEYSTONE_API_URL ||
+    "https://keystone-back.admin.serv.thiserv.com/api/graphql",
 });
 
 const cache = new InMemoryCache();
